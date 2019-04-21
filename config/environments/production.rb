77ddc3e75befedc8a8bc7c6247a96d7506ba656c
Yuki Yamato <ukitomato.developer@gmail.com>
2019-04-21 22:57:29 +0900

    FIx host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = ''
+  host = 'https://ancient-reef-41383.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
   Rails.application.routes.default_url_options[:host] = host
   ActionMailer::Base.smtp_settings = {
