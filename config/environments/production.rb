b0520dddfb3e09e1db58f257b8e1d44022c53da0
Yuki Yamato <ukitomato.developer@gmail.com>
2019-04-21 22:51:23 +0900

    SendGridのHostを空値に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://glacial-journey-34029.herokuapp.com'
+  host = ''
   config.action_mailer.default_url_options = {host: host}
   Rails.application.routes.default_url_options[:host] = host
   ActionMailer::Base.smtp_settings = {
