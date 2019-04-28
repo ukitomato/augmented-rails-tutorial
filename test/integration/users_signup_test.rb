3d09ae6d96e4f72a8be569b20c62ebe91c2b41c5
Yuki Yamato <ukitomato.developer@gmail.com>
2019-04-28 10:12:46 +0900

    テストの編集

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post users_path, params: {user: {name: "Example User",
                                        email: "user@example.com",
+                                       user_name: "example",
                                        password: "password",
                                        password_confirmation: "password"}}
     end
