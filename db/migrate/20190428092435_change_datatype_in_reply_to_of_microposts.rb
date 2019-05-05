8d1d439371ec86e21fefbe283c4e15eec475bb49
Yuki Yamato <ukitomato.developer@gmail.com>
2019-05-05 11:49:11 +0900

    不要なMigrationファイルの削除

--- db/migrate/20190428092435_change_datatype_in_reply_to_of_microposts.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeInReplyToOfMicroposts < ActiveRecord::Migration[5.1]
-  def change
-    change_column :microposts, :in_reply_to, :string
-  end
-end
--- db/migrate/20190505012655_change_datatype_in_reply_to_integer_of_microposts.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeInReplyToIntegerOfMicroposts < ActiveRecord::Migration[5.1]
-  def change
-    change_column :microposts, :in_reply_to, :integer
-  end
-end
